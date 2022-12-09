175e015e47fe4a9345611d83fb6491b4a01e403c
yueTX <501506969@qq.com>
2022-12-09 14:55:16 +0800

    提交废话文学项目

---
+++ build.js
@@ -0,0 +1,8 @@
+import { build } from "esbuild";
+const buildOptions = {
+    entryPoints:['./browser/index.js'],
+    outfile:'./dist/index.js',
+    bundle:true,
+    minify:true
+}
+build(buildOptions)
